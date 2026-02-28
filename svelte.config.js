102fdae0f49c9fbb270a4875b9a08d2d21e69cea
viorel nestor <nviorel7@protonmail.com>
2026-02-28 18:48:55 +0100

    Format svelte.config.js for better readability

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,15 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
   kit: {
-    adapter: adapter({ fallback: '404.html' }),
-    paths: { base: '/paginaialucas' },
-    prerender: { entries: ['*'] }
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: '/paginaialucas'
+    },
+    prerender: {
+      entries: ['*']
+    }
   }
 };
 
